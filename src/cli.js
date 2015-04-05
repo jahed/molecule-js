59ce62ec1ddf3385df91bfb134e3e707abed1f9a
Jahed <jahed.public+git@gmail.com>
2015-04-05 22:51:11 +0100

    Modularised project.

---
+++ src/cli.js
@@ -0,0 +1,38 @@
+var packageJson = require('../package.json'),
+    Molecule = require('./main'),
+    argv = require('minimist')(process.argv.slice(2), {
+        string: ['output'],
+        boolean: ['pretty', 'help', 'version'],
+        alias: {
+            'pretty': ['p'],
+            'output': ['o'],
+            'help': ['h'],
+            'version': ['v']
+        }
+    });
+
+if(argv.version) {
+    console.log(packageJson.name + ' ' + packageJson.version);
+    return;
+}
+
+if(argv.help) {
+    console.log();
+    console.log('molecule [options] <config directory>');
+    console.log();
+    console.log('Options:');
+    console.log('  --pretty, -p');
+    console.log('    Disables minification');
+    console.log('  --output, -o <build directory>');
+    console.log('    Changes ./build location.');
+    console.log('    Default: config directory');
+    console.log();
+    return;
+}
+
+var molecule = new Molecule(argv._[0], {
+    output: argv.output,
+    pretty: argv.pretty
+});
+
+molecule.build();
--- src/main.js
+++ src/main.js
@@ -4,143 +4,129 @@ var FeedParser = require('feedparser'),
     fs = require('fs'),
     path = require('path'),
     Promise = require('bluebird'),
-    packageJson = require('../package.json'),
-    argv = require('minimist')(process.argv.slice(2), {
-        string: ['output'],
-        boolean: ['pretty', 'help', 'version'],
-        alias: {
-            'pretty': ['p'],
-            'output': ['o'],
-            'help': ['h'],
-            'version': ['v']
-        }
-    });
+    packageJson = require('../package.json');
 
-if(argv.version) {
-    console.log(packageJson.name + ' ' + packageJson.version);
-    return;
-}
+var CONFIG_FILENAME = 'config.json';
 
-if(argv.help) {
-    console.log();
-    console.log('molecule [options] <config directory>');
-    console.log();
-    console.log('Options:');
-    console.log('  --pretty, -p');
-    console.log('    Disables minification');
-    console.log('  --output, -o <build directory>');
-    console.log('    Changes ./build location.');
-    console.log('    Default: config directory');
-    console.log();
-    return;
-}
+function Molecule(configDir, options) {
+    if(!this instanceof Molecule) {
+        throw new Error('Molecule is a constructor so call it with "new".');
+    }
 
-var configDir = argv._[0],
-    templateDir = path.join(configDir, './templates/'),
-    buildDir = argv.output ? argv.output : path.join(configDir, './build/');
+    this.config = JSON.parse(fs.readFileSync(path.join(configDir, CONFIG_FILENAME)));
 
-var config = JSON.parse(fs.readFileSync(path.join(configDir, './config.json')));
+    this.configDir = configDir;
+    this.templateDir = path.join(configDir, './templates/');
+    this.buildDir = options.output ? options.output : path.join(configDir, './build/');
 
-if(!fs.existsSync(buildDir)) {
-    fs.mkdirSync(buildDir);
-}
-
-var locals = {
-        pretty: argv.pretty,
+    this.locals = {
+        pretty: options.pretty,
         molecule: {
             version: packageJson.version,
             name: packageJson.name,
             url: packageJson.homepage
         },
-        authors: config.site.authors,
-        title: config.site.title,
-        description: config.site.description,
-        feeds: config.site.feeds,
+        authors: this.config.site.authors,
+        title: this.config.site.title,
+        description: this.config.site.description,
+        feeds: this.config.site.feeds,
         date: new Date(),
-        categories: config.site.categories,
-        contributors: config.sources.map(function(source) {
+        categories: this.config.site.categories,
+        contributors: this.config.sources.map(function(source) {
             return source.author;
         }),
         articles: []
     };
+}
 
-var promises = config.sources.map(function(source) {
-    return new Promise(function(resolve, reject) {
-        var req = request(source.url),
-            feedparser = new FeedParser(),
-            articlesAdded = 0;
+Molecule.prototype._parse = function _parse() {
+    return this.config.sources.map(function(source) {
+        return new Promise(function(resolve, reject) {
+            var req = request(source.url),
+                feedparser = new FeedParser(),
+                articles = [];
 
-        console.log('Getting feed for ' + source.url);
+            console.log('Getting feed for ' + source.url);
 
-        req.on('response', function (res) {
-            var stream = this;
+            req.on('response', function (res) {
+                var stream = this;
 
-            if (res.statusCode != 200) {
-                var err = new Error('Received status code ' + res.statusCode);
-                err.res = res;
-                return this.emit('error', err);
-            }
-
-            console.log('Got feed for ' + source.url);
-
-            stream.pipe(feedparser);
-        });
-
-        feedparser.on('readable', function() {
-            var stream = this,
-                meta = this.meta,
-                article;
-
-            while(article = stream.read()) {
-                if(source.limit && articlesAdded >= source.limit) {
-                    break;
+                if (res.statusCode != 200) {
+                    var err = new Error('Received status code ' + res.statusCode);
+                    err.res = res;
+                    return this.emit('error', err);
                 }
 
-                meta.date = meta.date || new Date();
-                meta.xmlurl = meta.xmlurl || source.url;
+                console.log('Got feed for ' + source.url);
 
-                article.author = article.author || source.author.name;
-                locals.articles.push(article);
-                articlesAdded++;
+                stream.pipe(feedparser);
+            });
+
+            feedparser.on('readable', function() {
+                var stream = this,
+                    meta = this.meta,
+                    article;
+
+                while(article = stream.read()) {
+                    if(source.limit && articles.length >= source.limit) {
+                        break;
+                    }
+
+                    meta.date = meta.date || new Date();
+                    meta.xmlurl = meta.xmlurl || source.url;
+
+                    article.author = article.author || source.author.name;
+                    articles.push(article);
+                }
+            });
+
+            feedparser.on('end', function() {
+                resolve(articles);
+            });
+
+            req.on('error', rejectOnError);
+            feedparser.on('error', rejectOnError);
+
+            function rejectOnError(err) {
+                err.source = source;
+                reject(err);
             }
         });
-
-        feedparser.on('end', function() {
-            resolve();
-        });
-
-        req.on('error', rejectOnError);
-        feedparser.on('error', rejectOnError);
-
-        function rejectOnError(err) {
-            err.source = source;
-            reject(err);
-        }
     });
-});
+};
 
-Promise.all(promises)
-    .then(function () {
-        var jadeExt = '.jade';
+Molecule.prototype.build = function build() {
+    return Promise
+        .reduce(this._parse(), function(allArticles, articles) {
+            return allArticles.concat(articles);
+        })
+        .then(function(articles) {
+            var jadeExt = '.jade';
 
-        locals.articles.sort(function(articleA, articleB) {
-            return articleB.date - articleA.date;
-        });
-
-        return fs.readdirSync(templateDir)
-            .filter(function(file) {
-                return path.extname(file) === jadeExt;
-            })
-            .map(function(file) {
-                var outputPath = path.join(buildDir, path.basename(file, jadeExt));
-                var output = jade.renderFile(path.join(templateDir, file), locals);
-
-                return Promise.promisify(fs.writeFile)(outputPath, output)
-                    .then(function() {
-                        console.log(outputPath + ' built.');
-                    });
+            articles.sort(function(articleA, articleB) {
+                return articleB.date - articleA.date;
             });
-    })
-    .catch(function(err) {
-        console.error(err);
-    });
+
+            this.locals.articles = articles;
+
+            if(!fs.existsSync(this.buildDir)) {
+                fs.mkdirSync(this.buildDir);
+            }
+
+            return fs.readdirSync(this.templateDir)
+                .filter(function(file) {
+                    return path.extname(file) === jadeExt;
+                })
+                .map(function(file) {
+                    var outputPath = path.join(this.buildDir, path.basename(file, jadeExt));
+                    var output = jade.renderFile(path.join(this.templateDir, file), this.locals);
+
+                    return Promise.promisify(fs.writeFile)(outputPath, output)
+                        .then(function() {
+                            console.log(outputPath + ' built.');
+                        });
+                }.bind(this));
+        }.bind(this));
+};
+
+module.exports = Molecule;
